db9d8cbae12dd45ff291a6db377a238e5bba8b8e
DavidYates1301 <64512123+DavidYates1301@users.noreply.github.com>
2025-05-28 13:54:35 +0700

    Create airbyte_apply.py

---
+++ airbyte_apply.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
+from airflow.providers.airbyte.sensors.airbyte import AirbyteJobSensor
+
+# Khởi tạo DAG
+with DAG(
+    dag_id='airbyte_sync',
+    start_date=days_ago(1),
+    schedule=None,  # Chạy thủ công hoặc trigger qua API Airflow
+    catchup=False
+) as dag:
+    # Task 1: Kích hoạt job Airbyte (async)
+    trigger_sync = AirbyteTriggerSyncOperator(
+        task_id='trigger_airbyte_sync',
+        airbyte_conn_id='airbyte_http',              # kết nối HTTP đã cấu hình
+        connection_id='ba78537a-fd02-4e98-8a8f-51f91672eee8', # ID Connection Airbyte
+        asynchronous=True                                    # chỉ trigger, không đợi ở đây
+    )
+    # Task 2: Sensor theo dõi job Airbyte đến khi hoàn tất
+    wait_for_sync = AirbyteJobSensor(
+        task_id='wait_for_airbyte_sync',
+        airbyte_conn_id='airbyte_http',
+        airbyte_job_id=trigger_sync.output                   # lấy job_id từ task trước
+    )
+    trigger_sync >> wait_for_sync
