5dd1d0dcc05709a69f62f7c9d92e4cedfa1347ab
DavidYates1301 <64512123+DavidYates1301@users.noreply.github.com>
2025-05-29 09:23:21 +0700

    Update airbyte_apply.py

--- airbyte_apply.py
+++ airbyte_apply.py
@@ -3,18 +3,28 @@ from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperat
 from airflow.providers.airbyte.sensors.airbyte import AirbyteJobSensor
 from datetime import datetime, timedelta
 
-# Khởi tạo DAG
 with DAG(
     dag_id='airbyte_sync',
     start_date=datetime(2023, 1, 1),
-    schedule=None,  # Chạy thủ công hoặc trigger qua API Airflow
+    schedule=None,  
     catchup=False
 ) as dag:
-    # Task 1: Kích hoạt job Airbyte (async)
-    trigger_sync = AirbyteTriggerSyncOperator(
+    trigger_sync_task1 = AirbyteTriggerSyncOperator(
         task_id='trigger_airbyte_sync',
-        airbyte_conn_id='airbyte_conn',              # kết nối HTTP đã cấu hình
-        connection_id='ba78537a-fd02-4e98-8a8f-51f91672eee8', # ID Connection Airbyte
+        airbyte_conn_id='airbyte_conn',              
+        connection_id='ba78537a-fd02-4e98-8a8f-51f91672eee8', 
         asynchronous=False                                    
     )
-    trigger_sync 
+    trigger_sync_task2 = AirbyteTriggerSyncOperator(
+        task_id='trigger_airbyte_sync',
+        airbyte_conn_id='airbyte_conn',              
+        connection_id='ea9ee394-f349-48f8-9fe2-393ff1fff9bf', 
+        asynchronous=False                                    
+    )
+    trigger_sync_task3 = AirbyteTriggerSyncOperator(
+        task_id='trigger_airbyte_sync',
+        airbyte_conn_id='airbyte_conn',              
+        connection_id='21d8b23f-161a-4cfa-b069-c4d1991e4bdc', 
+        asynchronous=False                                    
+    )
+    trigger_sync_task1 >>  trigger_sync_task2 >> trigger_sync_task3
