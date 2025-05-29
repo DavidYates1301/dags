b3a54869ff128272f9a400a1f91efa5f7c67b91d
DavidYates1301 <64512123+DavidYates1301@users.noreply.github.com>
2025-05-29 09:24:03 +0700

    Update airbyte_apply.py

--- airbyte_apply.py
+++ airbyte_apply.py
@@ -10,19 +10,19 @@ with DAG(
     catchup=False
 ) as dag:
     trigger_sync_task1 = AirbyteTriggerSyncOperator(
-        task_id='trigger_airbyte_sync',
+        task_id='trigger_airbyte_sync_1',
         airbyte_conn_id='airbyte_conn',              
         connection_id='ba78537a-fd02-4e98-8a8f-51f91672eee8', 
         asynchronous=False                                    
     )
     trigger_sync_task2 = AirbyteTriggerSyncOperator(
-        task_id='trigger_airbyte_sync',
+        task_id='trigger_airbyte_sync_2',
         airbyte_conn_id='airbyte_conn',              
         connection_id='ea9ee394-f349-48f8-9fe2-393ff1fff9bf', 
         asynchronous=False                                    
     )
     trigger_sync_task3 = AirbyteTriggerSyncOperator(
-        task_id='trigger_airbyte_sync',
+        task_id='trigger_airbyte_sync_3',
         airbyte_conn_id='airbyte_conn',              
         connection_id='21d8b23f-161a-4cfa-b069-c4d1991e4bdc', 
         asynchronous=False                                    
